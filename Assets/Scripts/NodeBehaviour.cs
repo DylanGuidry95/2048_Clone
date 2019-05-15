49ee117704fc269a3243f0c0db3be26d5a13eadb
Dylan Guidry <dylan.guidry@aie.edu.au>
2019-05-15 12:15:43 -0500

    Setup for material changes

--- Assets/Scripts/NodeBehaviour.cs
+++ Assets/Scripts/NodeBehaviour.cs
@@ -10,19 +10,44 @@ public class NodeBehaviour : MonoBehaviour
     [SerializeField]
     private Text ValueView;
 
-    public float MovementSpeed;
+    public float MovementSpeed;    
 
     private void Awake() 
     {        
         FaceValue = StartingValue[Random.Range(0,2)];
         ValueView = GetComponentInChildren<Text>();
-        ValueView.text = FaceValue.ToString();
+        ValueView.text = FaceValue.ToString();        
     }
 
     public void UpdateFaceValue(int value)
     {
         FaceValue = value;
         ValueView.text = FaceValue.ToString();
+        switch(FaceValue)
+        {
+            case 2:
+                break;
+            case 4:
+                break;
+            case 8:
+                break;
+            case 16:
+                break;
+            case 32:
+                break;
+            case 64:
+                break;
+            case 128:
+                break;
+            case 256:
+                break;
+            case 512:
+                break;
+            case 1024:
+                break;
+            case 2048:
+                break;
+        }
     }
 
     public void Move(Vector3 target)

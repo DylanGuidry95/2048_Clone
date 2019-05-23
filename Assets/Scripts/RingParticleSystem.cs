3c36c391b57e052e36807dfff1129cd88bf2a798
Dylan Guidry <guidry.dylan.95@gmail.com>
2019-05-23 14:54:45 -0500

    More fun with particles

---
+++ Assets/Scripts/RingParticleSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingParticleSystem : MonoBehaviour
+{
+    [Range(-1.0f,1.0f)]
+    public float XRotation;
+    [Range(-1.0f,1.0f)]
+    public float YRotation;
+    [Range(-1.0f,1.0f)]
+    public float ZRotation;
+
+    public float RotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotation = new Vector3(XRotation, YRotation, ZRotation) * RotationSpeed;
+        GetComponent<Rigidbody>().MoveRotation(GetComponent<Rigidbody>().rotation * Quaternion.Euler(rotation));
+    }
+}
